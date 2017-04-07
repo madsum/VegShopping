6a177aa520e4568a6c6da248dd2a5340781663e6
Muhammed Masum Islam <masum.islam@metropolia.fi>
2017-04-07 10:29:30 +0100

    This is is the initial commit. Just create project.

---
+++ src/main/java/com/ma/VegShoppingApplication.java
@@ -0,0 +1,12 @@
+package com.ma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegShoppingApplication.class, args);
+	}
+}
